a4fd67675b7710fc83037eecbea61d4f6c8f20e4
Miroslavetsh <dfgshte@gmail.com>
2021-12-07 16:09:32 +0200

    Finish sorting with popup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { Header, Categories } from './components'
+import { Header, Categories, SortPopup } from './components'
 import { ICategory } from './components/Category'
+import { ISortParameter } from './components/SortPopup'
 
 const App: React.FC = (): JSX.Element => {
   const categories: Array<ICategory> = [
@@ -11,6 +12,12 @@ const App: React.FC = (): JSX.Element => {
     { name: 'closed', text: 'Закрытые' },
   ]
 
+  const sortParameters: Array<ISortParameter> = [
+    ISortParameter.popularity,
+    ISortParameter.price,
+    ISortParameter.alphabet,
+  ]
+
   return (
     <div className='wrapper'>
       <Header />
@@ -20,30 +27,7 @@ const App: React.FC = (): JSX.Element => {
           <div className='content__top'>
             <Categories items={categories} />
 
-            <div className='sort'>
-              <div className='sort__label'>
-                <svg
-                  width='10'
-                  height='6'
-                  viewBox='0 0 10 6'
-                  fill='none'
-                  xmlns='http://www.w3.org/2000/svg'>
-                  <path
-                    d='M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z'
-                    fill='#2C2C2C'
-                  />
-                </svg>
-                <b>Сортировка по:</b>
-                <span>популярности</span>
-              </div>
-              <div className='sort__popup'>
-                <ul>
-                  <li className='active'>популярности</li>
-                  <li>цене</li>
-                  <li>алфавиту</li>
-                </ul>
-              </div>
-            </div>
+            <SortPopup items={sortParameters} />
           </div>
           <h2 className='content__title'>Все пиццы</h2>
           <div className='content__items'>
