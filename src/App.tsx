bac5a94509a6a469d5e07fdd1b47c7783c06f541
Miroslavetsh <dfgshte@gmail.com>
2021-12-07 20:35:33 +0200

    Make pizzas container & pizza blocks

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
+import { useEffect, useState } from 'react'
 import { Route } from 'react-router-dom'
 
 import { Header } from './components'
 import { Home, Cart } from './pages'
+import Pizza from './models/Pizza'
 
 const App: React.FC = (): JSX.Element => {
+  const [pizzas, setPizzas] = useState<Array<Pizza>>([])
+
+  useEffect(() => {
+    const getPizzas = async () => {
+      const response = await fetch('/db.json')
+      const { pizzas } = await response.json()
+
+      setPizzas(pizzas)
+    }
+
+    getPizzas()
+  }, [])
+
   return (
     <div className='wrapper'>
       <Header />
 
       <div className='content'>
-        <Route exact path='/' component={Home} />
+        <Route exact path='/' component={() => <Home pizzas={pizzas} />} />
         <Route exact path='/cart' component={Cart} />
       </div>
     </div>
